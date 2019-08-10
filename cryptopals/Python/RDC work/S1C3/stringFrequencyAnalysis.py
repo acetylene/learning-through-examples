ba448f22725602d6e022c3ffb29214096457de8c
Ryan Coatney <acetylene@users.noreply.github.com>
2019-08-10 01:11:42 -0700

    updating s1c3

--- cryptopals/Python/RDC work/S1C3/stringFrequencyAnalysis.py
+++ cryptopals/Python/RDC work/S1C3/stringFrequencyAnalysis.py
@@ -11,3 +11,7 @@ def frequency(message: str) -> dict:
         else:
             ret[key] = 1
     return ret
+# TODO: Create functions that run through the top (6) most frequent chars, xors and check to see if the xor'd string
+#  is all chars and whitespace
+# Remember, the most common character in a line of text is the space: ascii 32. if you have a char with a frequency
+#  higher than 12%, it's probably a chr(32)
